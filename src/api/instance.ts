304516920d31608fde2575a8fbbae917cbb23098
Kvaizer <kvaizer999@mail.ru>
2022-09-04 19:22:42 +0300

    'fix-headers'

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -5,15 +5,12 @@ export const login_instance = axios.create({
     baseURL: 'http://79.143.31.216/',
     transformRequest: (data: LoginRequestType) => {
         return `username=${data.username}&password=${data.password}`
+    },
+    headers: {
+        'Content-Type': 'application/json'
     }
 })
 
 export const instance = axios.create({
     baseURL: 'http://79.143.31.216/',
 })
-
-// instance.interceptors.request.use(value => {
-//     const token = localStorage.getItem('token')
-//     value.headers!.Authorization = `Bearer ${token}`
-//     return value
-// })
