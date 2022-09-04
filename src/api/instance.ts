3bc51e54ffe5794862caa6d0ebb5679a576c062b
Kvaizer <kvaizer999@mail.ru>
2022-09-04 19:33:03 +0300

    'fix-base-url'

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -2,14 +2,10 @@ import axios from 'axios';
 import {LoginRequestType} from './authAPI';
 
 export const login_instance = axios.create({
-    baseURL: 'http://79.143.31.216/',
+    baseURL: 'https://79.143.31.216/',
     transformRequest: (data: LoginRequestType) => {
         return `username=${data.username}&password=${data.password}`
     },
-    headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-    }
 })
 
 export const instance = axios.create({
