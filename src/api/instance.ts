eb27cf16823b4b1268d9e8b9a53e7ecf9df35a5e
Kvaizer <kvaizer999@mail.ru>
2022-09-04 20:00:22 +0300

    'fix2'

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -5,6 +5,10 @@ export const login_instance = axios.create({
     baseURL: 'http://79.143.31.216/',
     transformRequest: (data: LoginRequestType) => {
         return `username=${data.username}&password=${data.password}`
+    },
+    withCredentials: false,
+    headers: {
+        'content-type': 'application/x-www-form-urlencoded'
     }
 })
 
